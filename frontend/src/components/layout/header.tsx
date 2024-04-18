c0262120bc177f0fe4dcd7753a13df83e46595f6
Danzkreig <festeringwound@proton.me>
2024-04-18 16:32:24 +0800

    Added app to src and such.

--- frontend/src/components/layout/header.tsx
+++ frontend/src/components/layout/header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <div className="flex items-center justify-around h-16 bg-white w-full">
-      <h1>LazyCook</h1>
+      <h1 className="text-3xl font-bold">LazyCook</h1>
       {/* this is the link list */}
       <div className="flex items-center justify-center gap-5">
         <a>Home</a>
