6162ce95a2f616179be80a25c25dc9f8e5c48e79
Bayr-Erdene Enkhtur <23LP1331@G401-CC0101.local>
2024-04-20 19:53:00 +0800

    asdad

---
+++ frontend/src/components/filterCard/IngredientsCard.tsx
@@ -0,0 +1,40 @@
+import { MdKeyboardArrowDown } from "react-icons/md";
+import { Ingredient } from "./Ingredient";
+export const IngredientsCard = ({
+  title,
+  ingNumber,
+  img,
+}: {
+  title: string;
+  ingNumber: number;
+  img: string;
+}) => {
+  return (
+    <div className="h-fit w-[90%] border-[3px] shadow-lg rounded-lg mt-[3%]">
+      <div className="flex justify-between w-full h-[30%]">
+        <img src={img} alt="" className=" rounded-lg" />
+        <div className="flex flex-col justify-center items-center">
+          <div className=" font-light text-[19px]">{title}</div>
+          <div className=" text-gray-400 text-[23px]">
+            {ingNumber}/40 Ingredients
+          </div>
+        </div>
+        <div>
+          <MdKeyboardArrowDown className="w-[30px] h-[30px]" />
+        </div>
+      </div>
+      <hr />
+      <div className="flex w-full h-fit p-[20px] flex-wrap gap-[2%]">
+        <Ingredient title={"butter"} />
+        <Ingredient title={"butter"} />
+        <Ingredient title={"butter"} />
+        <Ingredient title={"butter"} />
+        <Ingredient title={"butter"} />
+        <Ingredient title={"butter"} />
+        <Ingredient title={"butter"} />
+        <Ingredient title={"butter"} />
+        <Ingredient title={"butter"} />
+      </div>
+    </div>
+  );
+};
